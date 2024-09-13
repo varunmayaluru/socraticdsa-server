b6935f3ecd6d22bcc0739fa43ae0a412983a528a
varunmayaluru <varun.mjrph@gmail.com>
2024-09-13 14:13:59 +0530

    get available port

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routers import problem_routes, openai_routes
+import os
 
 app = FastAPI()
 
@@ -10,4 +11,6 @@ app.include_router(openai_routes.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, loop="asyncio")
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, loop="asyncio")
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
