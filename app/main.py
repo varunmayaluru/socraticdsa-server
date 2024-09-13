ce9552319c77ada4207940de44d03da65fbd3441
varunmayaluru <varun.mjrph@gmail.com>
2024-09-13 13:01:24 +0530

    loop asyncio added to main for loop handler

--- app/main.py
+++ app/main.py
@@ -10,4 +10,4 @@ app.include_router(openai_routes.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, loop="asyncio")
